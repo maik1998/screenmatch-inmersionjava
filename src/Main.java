24115f8043ffb5a8596236c8a78e9b2014066785
Michael <70036874+maik1998@users.noreply.github.com>
2024-05-08 13:11:20 -0500

    Aula 02 - Finalización Proyecto (Implementación de modelos e interfaces y logica de negocio)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 
 public class Main {
+    /**
+     * Main de la aplicación
+     * @param args
+     */
     public static void main (String [] args){
         App app = new App();
         app.muestraElMenu();
